50ab0be90b416bac6ec6fe728b14707db24c28f5
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-12 23:27:41 +0900

    initial commit

---
+++ src/main/java/core/advanced/trace/TraceId.java
@@ -0,0 +1,34 @@
+package core.advanced.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    public String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+}
