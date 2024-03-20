a428ac2f392e2e007df9839ee3d87b60d63d1bcd
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-20 23:42:22 +0900

    프록시 패턴 -예제 코드1
    이미 개발된 로직을 전혀 수정하지않고, 프록시 객체를 통해서 캐시를 적용해 보자

---
+++ src/test/java/core/advanced/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,17 @@
+package core.advanced.pureproxy.proxy;
+
+import core.advanced.pureproxy.proxy.code.ProxyPatternClient;
+import core.advanced.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
---
+++ src/test/java/core/advanced/pureproxy/proxy/code/Subject.java
@@ -0,0 +1,5 @@
+package core.advanced.pureproxy.proxy.code;
+
+public interface Subject {
+    String operation();
+}
