d79eceabe60295872c787b758e12a8f58772197a
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-20 23:52:50 +0900

    프록시 패턴 -예제 코드2
    프록시를 도입해서 접근 제어를 했다

--- src/test/java/core/advanced/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/core/advanced/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package core.advanced.pureproxy.proxy;
 
+import core.advanced.pureproxy.proxy.code.CacheProxy;
 import core.advanced.pureproxy.proxy.code.ProxyPatternClient;
 import core.advanced.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,17 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        // real이 아니라 proxy을 넣어준다. ProxyPatternClient는 내부에 Subject라는 인터페이스만 의존하기 때문에 실제 객체인지 프록시인지 알지 못한다.
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
