ae8e32607cbfd53a7aa2b70cfe6544bac0d27fd1
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-20 22:48:15 +0900

    예제 프로젝트 만들기 v1
    스프링빈 수동등록이라 컴포넌트 스캔의 대상이 되면 안된다

--- src/main/java/core/advanced/CoreApplication.java
+++ src/main/java/core/advanced/CoreApplication.java
@@ -1,9 +1,12 @@
 package core.advanced;
 
+import core.advanced.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "core.advanced.app.v3")
 public class CoreApplication {
 
     public static void main(String[] args) {
