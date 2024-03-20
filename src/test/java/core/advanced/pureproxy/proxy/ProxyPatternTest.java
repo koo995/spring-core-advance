1187c4e24cbdb5df92eff5d1b33ea8f432d8b160
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-21 00:34:19 +0900

    프록시 패턴과 데코레이터 패턴 정리. 의도에 따라 구분

--- src/test/java/core/advanced/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/core/advanced/pureproxy/proxy/ProxyPatternTest.java
@@ -20,7 +20,10 @@ public class ProxyPatternTest {
     void cacheProxyTest() {
         RealSubject realSubject = new RealSubject();
         CacheProxy cacheProxy = new CacheProxy(realSubject);
-        // real이 아니라 proxy을 넣어준다. ProxyPatternClient는 내부에 Subject라는 인터페이스만 의존하기 때문에 실제 객체인지 프록시인지 알지 못한다.
+        /**
+         real이 아니라 proxy을 넣어준다. ProxyPatternClient는 내부에 Subject라는 인터페이스만 의존하기 때문에 실제 객체인지 프록시인지 알지 못한다.
+         접근을 제어한다고 볼 수 있다
+         */
         ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
