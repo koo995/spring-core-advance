80d24e637081270f9f7d47706774d46139dc4d98
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-21 22:16:52 +0900

    fix starting error

--- src/main/java/core/advanced/CoreApplication.java
+++ src/main/java/core/advanced/CoreApplication.java
@@ -1,16 +1,12 @@
 package core.advanced;
 
-import core.advanced.config.v1_proxy.ConcreteProxyConfig;
-import core.advanced.config.v1_proxy.InterfaceProxyConfig;
 import core.advanced.trace.logtrace.LogTrace;
 import core.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 //@Import(value = {AppV1Config.class, AppV2Config.class})
-@Import({ConcreteProxyConfig.class, InterfaceProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "core.advanced.app.v3")
 public class CoreApplication {
 
