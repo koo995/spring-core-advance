73e6a8aabe1a1b60766f8b973c4af0753988e247
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-21 18:37:23 +0900

    jdk 동적 프록시 -소개
    주의: jdk 동적 프록시는 인터페이스를 기반으로 프록시를 동적으로 만들어준다. 따라서 인터페이스가 필수이다.

---
+++ src/test/java/core/advanced/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package core.advanced.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/core/advanced/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package core.advanced.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
