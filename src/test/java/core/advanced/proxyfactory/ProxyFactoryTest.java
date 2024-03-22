1f8bce5a73cb62826afeed6c2c1d02f16546eb89
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-22 14:40:42 +0900

    프록시 팩토리 -예제 코드1

---
+++ src/test/java/core/advanced/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,44 @@
+package core.advanced.proxyfactory;
+
+import core.advanced.common.advice.TimeAdvice;
+import core.advanced.common.service.ServiceImpl;
+import core.advanced.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @DisplayName("인터페이스가 있으면 jdk 동적 프록시 사용")
+    @Test
+    void interfaceProxy() throws Exception {
+        // given
+        ServiceInterface target = new ServiceImpl();
+        /**
+         * 타겟넣으면서 프록시 팩토리를 만들면 된다.
+         */
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        /**
+         * proxyClass=class jdk.proxy3.$Proxy12 >> jdk 동적 프록시가 생성되었다.
+         */
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.save();
+
+        /**
+         * 프록시 팩토리로 프록시가 잘 적용되었는지 확인하려면 다음 기능을 사용하면 된다.
+         * 프록시 팩토리를 쓸때만 AopUtils 을 쓸 수 있다.
+         */
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
