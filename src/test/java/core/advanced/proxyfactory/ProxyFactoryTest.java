e79bcb6813514921c0937d838c5d9c1eff87f629
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-22 14:51:11 +0900

    프록시 팩토리 -예제 코드2
    참고로 스프링부트는 Aop를 적용할 때 기본적으로 setProxyTargetClass를 true로 사용한다. 인터페이스가 있던 없던 항상 CGLIB 를 사용해서 구체 클래스를 기반으로 프록시를 생성한다.

--- src/test/java/core/advanced/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/core/advanced/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package core.advanced.proxyfactory;
 
 import core.advanced.common.advice.TimeAdvice;
+import core.advanced.common.service.ConcreteService;
 import core.advanced.common.service.ServiceImpl;
 import core.advanced.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -41,4 +42,61 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
     }
+
+    @DisplayName("구체클래스만 있으면 CGLIB 사용")
+    @Test
+    void concreteProxy() throws Exception {
+        // given
+        ConcreteService target = new ConcreteService();
+        /**
+         * 타겟넣으면서 프록시 팩토리를 만들면 된다.
+         */
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        /**
+         * proxyClass=class jdk.proxy3.$Proxy12 >> jdk 동적 프록시가 생성되었다.
+         */
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.call();
+
+        /**
+         * 프록시 팩토리로 프록시가 잘 적용되었는지 확인하려면 다음 기능을 사용하면 된다.
+         * 프록시 팩토리를 쓸때만 AopUtils 을 쓸 수 있다.
+         */
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 을 사용하고, 클래스 기반 프록시 사용")
+    @Test
+    void proxyTargetClass() throws Exception {
+        // given
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        /**
+         * 인터페이스가 있던 없던 항상 CGLIB을 사용하고 싶다면
+         * 실무에서도 등장하기 때문에 중요하다.
+         */
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        /**
+         * proxyClass=class jdk.proxy3.$Proxy12 >> jdk 동적 프록시가 생성되었다.
+         */
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.save();
+
+        /**
+         * 프록시 팩토리로 프록시가 잘 적용되었는지 확인하려면 다음 기능을 사용하면 된다.
+         * 프록시 팩토리를 쓸때만 AopUtils 을 쓸 수 있다.
+         */
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
 }
