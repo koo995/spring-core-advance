a222ad32dbfb959c6d7c9da58fe88a9c5dfca08a
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-23 23:48:28 +0900

    스프링이 제공하는 빈 후처리기1
    중요: 포인트컷은 2가지에 사용된다.
    1. 프록시 적용 여부 판단 -생성 단계
    자동 프록시 생성기는 포인트컷을 사용해서 해당 빈이 프록시를 생성할 필요가 있는지 없는지 체크한다.
    클래스 + 메서드 조건을 모두 비교한다. 이때 모든 메서드를 체크하는데, 포인터컷 조건에 하나하나 매칭해본다. 만약 조건에 맞는 것이 하나라도 있으면 프록시를 생성한다.
    예) 'orderControllerV1' 은 'request()', 'noLog()' 가 있다. 'request()' 가 조건에 만족하므로 프록시를 생성한다.
    2. advice 적용 여부 판단 -사용 단계
    프록시가 호출되었을 때 부가 기능인 advice 를 적용할지 말지 포인트컷을 보고 판단한다.
    앞선 예에서 'orderControllerV1' 은 이미 프록시가 걸려있다. 'orderControllerV1'의 'request()'는 현재 포인트컷 조건에 만족하므로 프록시는 advice 를 먼저 호출하고, target 을 호출한다. 'noLog()' 는 advice 를 호출하지 않고 바로 'target' 만 호출한다.

--- src/main/java/core/advanced/CoreApplication.java
+++ src/main/java/core/advanced/CoreApplication.java
@@ -1,6 +1,6 @@
 package core.advanced;
 
-import core.advanced.config.postprocessor.BeanPostProcessorConfig;
+import core.advanced.config.autoproxy.AutoProxyConfig;
 import core.advanced.trace.logtrace.LogTrace;
 import core.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "core.advanced.app.v3")
 public class CoreApplication {
 
