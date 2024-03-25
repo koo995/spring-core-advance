1850f3c3e7921014b7ecc0f9ba266c50bbb4955f
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-25 13:38:44 +0900

    예제 프로젝트 만들기

--- src/main/java/core/advanced/CoreApplication.java
+++ src/main/java/core/advanced/CoreApplication.java
@@ -1,26 +1,13 @@
 package core.advanced;
 
-import core.advanced.config.aop.AopConfig;
-import core.advanced.trace.logtrace.LogTrace;
-import core.advanced.trace.logtrace.ThreadLocalLogTrace;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
-@Import(AopConfig.class)
-@SpringBootApplication(scanBasePackages = "core.advanced.app.v3")
+@SpringBootApplication()
 public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
-    }
-
-    /**
-     * basePackage 위치랑 떨어져 있는데 이건 적용되는 것인가.
-     */
-    @Bean
-    public LogTrace logTrace() {
-        return new ThreadLocalLogTrace();
     }
 }
---
+++ src/test/java/core/advanced/AopTest.java
@@ -0,0 +1,43 @@
+package core.advanced;
+
+
+import core.advanced.order.OrderRepository;
+import core.advanced.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    /**
+     * aop 가 적용이 되었는지 안되었는지 확인하기 위함.
+     */
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
